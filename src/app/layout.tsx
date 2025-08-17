d3907892d2e669d39f28bf1202fa8c7c44b3e951
Kamlesh kumar <kamlesh.kumar@trackolap.com>
2025-08-17 21:48:46 +0530

    changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,9 +6,8 @@ import Providers from "./providers";
 import Navbar from "./components/navbar";
 
 export const metadata: Metadata = {
-  title: "v0 App",
-  description: "Created with v0",
-  generator: "v0.dev",
+  title: "Cloudship",
+  description: "Cloudship",
 };
 
 export default async function RootLayout({ children }: { children: React.ReactNode }) {
